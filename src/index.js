455e716bdcffe97ae1adb02c333019aa4cd33662
chanjeong park <ckswjd9595@gmail.com>
2024-12-18 16:56:22 +0900

    Vanilla-Redux 실행시켜보기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'Add':
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case 'Delete':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const todoStore = createStore(todoReducer);
+
+const dispatchAddTodo = text => {
+  todoStore.dispatch({ type: 'Add', text: text });
+};
+
+const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  todoStore.dispatch({ type: 'Delete', id: id });
+};
+
+const printTodo = () => {
+  const todos = todoStore.getState();
+  ul.innerHTML = '';
+  todos.forEach(todo => {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    deleteButton.addEventListener('click', dispatchDeleteTodo);
+    li.id = todo.id;
+    li.innerText = todo.text;
+    deleteButton.innerText = 'Delete';
+    li.appendChild(deleteButton);
+    ul.appendChild(li);
+  });
+};
+todoStore.subscribe(printTodo);
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  dispatchAddTodo(toDo);
+};
+
+form.addEventListener('submit', onSubmit);
